866c921fc2ab08222ecbdb72b99e5788abf5a3d6
Adam Moreno <abeangoalie39@gmail.com>
2019-12-14 11:03:23 -0600

    Inventeory view controller

--- Inventory/Inventory/Controller/InventoryViewController.swift
+++ Inventory/Inventory/Controller/InventoryViewController.swift
@@ -9,22 +9,58 @@
 import UIKit
 
 class InventoryViewController: UIViewController {
+    
+    lazy var scannerView: ScannerView = {
+        let view = ScannerView()
+        
+        return view
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let inventoryStackView = self.view.subviews[0] as! UIStackView
+        
+        inventoryStackView.backgroundColor = .purple
+        
+        let navBar = inventoryStackView.subviews[0] as! InventoryNavigationBar
+        navBar.inventoryDelegate = self
+        
+        let tableView = inventoryStackView.subviews[1] as! InventoryTableView
+        tableView.delegate = inventoryStackView.subviews[1] as! InventoryTableView
+        tableView.dataSource = inventoryStackView.subviews[1] as! InventoryTableView
+        
+        let tabBar = inventoryStackView.subviews[2] as! InventoryTabBar
+        tabBar.delegate = inventoryStackView.subviews[2] as! InventoryTabBar
+        tabBar.inventoryDelegate = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension InventoryViewController: InventoryNavBarDelegate {
+    func exitApplication() {
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension InventoryViewController: InventoryTabBarDelegate {
+    func bringForwardInventoryTableView() {
+        self.view.sendSubviewToBack(scannerView)
+    }
+    
+    func bringForwardScannerView() {
+        let scan = ScannerView()
+        self.view.insertSubview(scan, at: 0)
+    }
+    
+    func bringForwardShoppingCart() {
+        print("Shopping cart view controller")
+    }
+    
+    
 }
