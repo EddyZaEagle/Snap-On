8f239d8986453003fce7b2d751373fcd567106a3
Adam Moreno <abeangoalie39@gmail.com>
2019-12-14 13:58:27 -0600

    UI in code

--- Inventory/Inventory/View/ScannerView.swift
+++ Inventory/Inventory/View/ScannerView.swift
@@ -14,19 +14,48 @@ protocol ScannerViewDelegate {
 
 class ScannerView: UIView {
 
-    @IBOutlet weak var btnEnter: UIButton!
-    @IBOutlet weak var txtWMS: UITextField!
+    @IBOutlet weak var enter: UIButton!
+    @IBOutlet weak var wms: UITextField!
+    
+    lazy var txtWMS: UITextField = {
+        let text = UITextField(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        text.layer.borderWidth = 2.5
+        text.layer.borderColor = UIColor.black.cgColor
+        text.layer.cornerRadius = 5
+        text.textAlignment = .center
+        text.attributedPlaceholder = NSAttributedString(string: "Enter WMS", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)])
+        text.tag = 111
+        text.backgroundColor = .snapOnGray
+        text.textColor = .white
+        return text
+    }()
+    
+    lazy var btnEnter: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        button.backgroundColor = .snapOnRed
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 30
+        button.layer.borderWidth = 2.5
+        button.layer.borderColor = UIColor.black.cgColor
+        button.setTitle("Enter", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.addTarget(self, action: #selector(enterButton), for: .touchUpInside)
+        return button
+    }()
     
     var scannerDelegate: ScannerViewDelegate? // refrence to protocol
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpScannerView()
         
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        
     }
     
     override func layoutSublayers(of layer: CALayer) {
@@ -37,7 +66,7 @@ class ScannerView: UIView {
     /**
      Adopting and implementing delegate to check if wms text blongs to an employee at snapon
      */
-    @IBAction private func enterButton() {
+    @objc fileprivate func enterButton() {
         if let delegate = scannerDelegate, let wmsText = txtWMS.text {
             if (wmsText.checkWMS) {
                 delegate.findEmployee(wms: wmsText)
@@ -50,6 +79,8 @@ class ScannerView: UIView {
      Set up scanner's view
      */
     private func setUpScannerView() {
+        self.addSubview(txtWMS)
+        self.addSubview(btnEnter)
         setUpWMS_Label()
         setUpEnterButton()
     }
@@ -59,29 +90,25 @@ class ScannerView: UIView {
      */
     private func setUpEnterButton() {
         btnEnter.translatesAutoresizingMaskIntoConstraints = false
-        btnEnter.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        btnEnter.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        btnEnter.backgroundColor = .snapOnRed
-        btnEnter.setTitleColor(.black, for: .normal)
-        btnEnter.layer.cornerRadius = 30
-        btnEnter.layer.borderWidth = 2.5
-        btnEnter.layer.borderColor = UIColor.black.cgColor
-        btnEnter.setTitle("Enter", for: .normal)
-        btnEnter.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        btnEnter.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        btnEnter.topAnchor.constraint(equalTo: txtWMS.bottomAnchor, constant: 100).isActive = true
     }
     
     /**
      Set up wms's label
      */
     private func setUpWMS_Label() {
-        txtWMS.tag = 111
-        txtWMS.layer.cornerRadius = 5
-        txtWMS.layer.borderWidth = 2.5
-        txtWMS.layer.borderColor = UIColor.black.cgColor
-        txtWMS.backgroundColor = .snapOnGray
-        txtWMS.textColor = .white
-        txtWMS.attributedPlaceholder = NSAttributedString(string: "Enter WMS", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)])
-        txtWMS.textAlignment = .center
+        txtWMS.translatesAutoresizingMaskIntoConstraints = false
+        
+        txtWMS.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        txtWMS.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        txtWMS.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        txtWMS.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 100).isActive = true
+        
+        txtWMS.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -100).isActive = true
     }
 
 }
