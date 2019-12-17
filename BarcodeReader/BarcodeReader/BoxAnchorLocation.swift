c80721651829f8d4a52956a8e3c81bfd4dc1d524
Adam Moreno <abeangoalie39@gmail.com>
2019-12-17 09:27:12 -0600

    only reads QR code

---
+++ BarcodeReader/BarcodeReader/BoxAnchorLocation.swift
@@ -0,0 +1,17 @@
+//
+//  BoxAnchorLocation.swift
+//  BarcodeReader
+//
+//  Created by Adam Moreno on 12/16/19.
+//  Copyright © 2019 Adam Moreno. All rights reserved.
+//
+
+import Foundation
+
+enum BoxAnchorLocation {
+    case bottomLeft
+    case topLeft
+    case topRight
+    case bottomRight
+    case none
+}
