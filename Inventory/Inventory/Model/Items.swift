1cb2cfb082c5ad9a421a84a5d58349a4bae2f9b2
Adam Moreno <abeangoalie39@gmail.com>
2019-12-20 15:29:46 -0600

    Items file

---
+++ Inventory/Inventory/Model/Items.swift
@@ -0,0 +1,41 @@
+//
+//  Items.swift
+//  Inventory
+//
+//  Created by Adam Moreno on 12/20/19.
+//  Copyright © 2019 Adam Moreno. All rights reserved.
+//
+
+import Foundation
+
+protocol ItemsProtocol {
+    var id: String { get }
+    var item: String { get }
+    var qty: Double { get }
+    init(id: String, item: String, qty: Double)
+}
+
+class ItemsClass: ItemsProtocol {
+    
+    private var _id: String
+    private var _item: String
+    private var _qty: Double
+    
+    var id: String {
+        get { return _id }
+    }
+    
+    var item: String {
+        get { return _item }
+    }
+    
+    var qty: Double {
+        get { return _qty }
+    }
+    
+    required init(id: String, item: String, qty: Double) {
+        _id = id
+        _item = item
+        _qty = qty
+    }
+}
